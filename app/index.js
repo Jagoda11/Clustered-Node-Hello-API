924593d5db721ab68b54481805e28970121bfa07
JagodaCubrilo <jagoda.cubrilo@yahoo.com>
2021-03-30 16:50:37 +0200

    adding cluster module

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const cluster = require('cluster');
+const os = require('os');
 
 function serverHandler(req, res) {
   req.url = req.url;
@@ -11,4 +13,25 @@ function serverHandler(req, res) {
   res.end();
 }
 
-http.createServer(serverHandler).listen(3000);
+function serverHand(req, res) {
+  req.url = req.url;
+  if (req.url === '/hello-again') {
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({ message: 'well:  /hello-again' }));
+  } else {
+    res.write('Hello to you too!');
+  }
+  res.end();
+}
+
+//master thread
+if (cluster.isMaster) {
+  http.createServer(serverHandler).listen(3000);
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+} else {
+  http.createServer(serverHand).listen(3001);
+  //if not on master thread,add something here
+  // add something here
+}
